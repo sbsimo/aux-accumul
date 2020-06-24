51bfc2e354c7a081f3c51be14ee6acdd313f07e2
sbsimo <simone.balbo@ithaca.polito.it>
2020-06-24 15:25:02 +0200

    document WRF-reader class

--- wrfita_aux.py
+++ wrfita_aux.py
@@ -1,3 +1,4 @@
+"""Define a class for reading WRF data"""
 import os
 import datetime
 
@@ -7,10 +8,37 @@ from osgeo import gdal, osr
 
 
 class WrfItaAux:
+    """A class used to read WRF data
+
+    Attributes:
+        abspath: str
+            the absolute path to the file on disk in the os.path style
+        dirname: str
+            the absolute path to the parent directory containing the
+            WRF file
+        basename: str
+            the filename of this WRF file
+        period: datetime.timedelta
+            the period of time this file refers to with respect to
+            January 1st, 2000
+        end_dt: datetime.datetime
+            the instant in time corresponding to the end of the
+            observation
+        start_dt: datetime.datetime
+            the instant in time corresponding to the start of the
+            observation
+        model_run_dt: datetime.datetime
+            the instant in time corresponding to the run of the
+            model
+    """
     EPSG_CODE = 4326
     FILENAME_FORMAT = 'sft_rftm_rg_wrfita_aux_d02_%Y-%m-%d_00_*'
 
     def __init__(self, abspath):
+        """
+        :param abspath: str
+            absolute path to the file on disk in the os.path style
+        """
         self.abspath = abspath
         self.dirname, self.basename = os.path.split(abspath)
         with Dataset(abspath) as ds:
@@ -47,6 +75,16 @@ class WrfItaAux:
         return self.start_dt > other.start_dt
 
     def __lt__(self, other):
+        """Compare the current WrfItaAux object with another.
+
+        Return true if the instant of initial time corresponding to the
+        other object comes after the instant of initial time of the
+        current object.
+
+        :param other: WrfItaAux
+            the other object in comparison
+        :return: bool
+        """
         return self.start_dt < other.start_dt
 
     @property
@@ -63,18 +101,36 @@ class WrfItaAux:
 
     @property
     def x_max(self):
+        """Get the maximum longitude value.
+
+        :return: float
+            A floating point number corresponding to the maximum
+            longitude value
+        """
         if self._x_max is None:
             self._x_max = self.lons[-1]
         return self._x_max
 
     @property
     def y_min(self):
+        """Get the minimum latitude value.
+
+        :return: float
+            A floating point number corresponding to the minimun
+            latitude value
+        """
         if self._y_min is None:
             self._y_min = self.lats[0]
         return self._y_min
 
     @property
     def y_max(self):
+        """Get the maximum latitude value.
+
+        :return: float
+            A floating point number corresponding to the maximum
+            latitude value
+        """
         # TODO produce tests for these
         if self._y_max is None:
             self._y_max = self.lats[-1]
@@ -82,7 +138,7 @@ class WrfItaAux:
 
     @property
     def pixel_size_x(self):
-        """Get the pixel size in along the longitude axis.
+        """Get the pixel size along the longitude axis.
 
         :return: float
         """
@@ -92,6 +148,10 @@ class WrfItaAux:
 
     @property
     def pixel_size_y(self):
+        """Get the pixel size along the latitude axis.
+
+        :return: float
+        """
         # TODO implement test for these
         if self._pixel_size_y is None:
             self._pixel_size_y = (self.y_max - self.y_min)/(len(self.lats) - 1)
@@ -99,6 +159,14 @@ class WrfItaAux:
 
     @property
     def geotransform(self):
+        """Get the affine geotransform coefficients.
+
+        Generate the affine geotransform coefficients according to
+        https://gdal.org/user/raster_data_model.html#affine-geotransform
+
+        :return: tuple
+            containing the six coefficients
+        """
         if self._geotransform is None:
             self._geotransform = (self.x_min - self.pixel_size_x / 2, self.pixel_size_x, 0,
                                   self.y_min - self.pixel_size_y / 2, 0, self.pixel_size_y)
@@ -126,6 +194,15 @@ class WrfItaAux:
 
     @property
     def rainnc(self):
+        """Get the array of RAINNC values.
+
+        Open the netCDF4 file on disk and read the RAINNC variable, as
+        well as the NoData value for it.
+
+        :return: numpy.ndarray
+        :raise: OSError
+            in case the variable cannot be read.
+        """
         try:
             with Dataset(self.abspath) as ds:
                 rainnc = ds.variables['RAINNC'][0]
@@ -137,10 +214,25 @@ class WrfItaAux:
 
     @property
     def rain(self):
+        """Get the total precipitation values.
+
+        The total precipitation is calculated as the sum of the
+        RAINC and RAINNC variables.
+
+        :return: numpy.ndarray
+        """
         return self.rainc + self.rainnc
 
     @property
     def lats(self):
+        """Get the array of latitude values.
+
+        Open the netCDF4 file on disk and read the lat variable.
+
+        :return: numpy.ndarray
+        :raise: OSError
+            in case the variable cannot be read.
+        """
         try:
             with Dataset(self.abspath) as ds:
                 xs = ds.variables['lat'][:]
@@ -151,6 +243,14 @@ class WrfItaAux:
 
     @property
     def lons(self):
+        """Get the array of longitude values.
+
+        Open the netCDF4 file on disk and read the lon variable.
+
+        :return: numpy.ndarray
+        :raise: OSError
+            in case the variable cannot be read.
+        """
         try:
             with Dataset(self.abspath) as ds:
                 ys = ds.variables['lon'][:]
@@ -161,6 +261,13 @@ class WrfItaAux:
 
     @property
     def no_data(self):
+        """Get the NoData value from the netCDF original file.
+
+        :return: float
+        :raise: Value Error
+            in case the NoData value for the RAINC and for the RAINNC
+            differ from each other
+        """
         if self._no_data is None:
             if self._no_data_rainc is None:
                 self.rainc
